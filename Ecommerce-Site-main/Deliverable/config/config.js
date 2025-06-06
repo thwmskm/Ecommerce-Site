a19196f589833a049764056e4849c673cf8db510
Thomas Kim <thomas.san03@gmail.com>
2025-06-06 12:04:13 -0400

    config changes

--- Ecommerce-Site-main/Deliverable/config/config.js
+++ Ecommerce-Site-main/Deliverable/config/config.js
@@ -1,26 +1,31 @@
+require("dotenv").config();
+
 module.exports = {
   development: {
-    username: "caradmin",
-    password: "AdminStrongPass2025!",
-    database: "carstore",
-    host: "carstore-db.cvggumuwm4u3.ca-central-1.rds.amazonaws.com",
-    dialect: "mysql"
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql",
   },
   production: {
-    username: "caradmin",
-    password: "AdminStrongPass2025!",
-    database: "carstore",
-    host: "carstore-db.cvggumuwm4u3.ca-central-1.rds.amazonaws.com",
-    dialect: "mysql",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
     dialectOptions: {
-      ssl: 'Amazon RDS'
-    }
-  }
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
