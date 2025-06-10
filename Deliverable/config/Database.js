5ba90574e91c22bcda69a7e0c94c34f0e23f2e1b
Thomas Kim <thomas.san03@gmail.com>
2025-06-09 20:29:06 -0400

    cloud db config

--- Deliverable/config/Database.js
+++ Deliverable/config/Database.js
@@ -1,6 +1,5 @@
-
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -9,16 +8,17 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    logging: false
+    port: process.env.DB_PORT || 3306,
+    logging: false,
   }
 );
 
 const conn = async () => {
   try {
     await sequelize.authenticate();
-    console.log('✅ Connection has been established successfully.');
+    console.log("✅ Connection has been established successfully.");
   } catch (error) {
-    console.error('❌ Unable to connect to the database:', error);
+    console.error("❌ Unable to connect to the database:", error);
   }
 };
 
