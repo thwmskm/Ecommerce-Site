131fe9111fad8663f283cee52fb4cfee89b3e176
Thomas Kim <thomas.san03@gmail.com>
2025-06-19 20:06:55 -0400

    CORS edit

--- Deliverable/app.js
+++ Deliverable/app.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 
 const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN;
 
-app.use(cors({ origin: FRONTEND_ORIGIN || 5173, credentials: true }));
+app.use(cors({ origin: FRONTEND_ORIGIN, credentials: true }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -87,6 +87,7 @@ app.get("/api/cart", (req, res) => {
       0
     ),
   });
+  console.log("Session Cookie:", req.headers.cookie);
   console.log("SESSION ID:", req.sessionID);
 });
 
