d05c29401de0faf276a73ccbf08328b55fdb771f
Thomas Kim <thomas.san03@gmail.com>
2025-09-15 14:53:39 -0400

    FINAL COMMIT

--- Deliverable/Models/ItemSold.js
+++ Deliverable/Models/ItemSold.js
@@ -1,37 +1,42 @@
 // models/ItemSold.js
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/Database');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/Database");
 
-const ItemSold = sequelize.define('ItemSold', {
-  id: {
-    type: DataTypes.BIGINT.UNSIGNED,
-    autoIncrement: true,
-    primaryKey: true,
+const ItemSold = sequelize.define(
+  "ItemSold",
+  {
+    id: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    vid: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      references: {
+        model: "Vehicles",
+        key: "vid",
+      },
+      onDelete: "CASCADE",
+    },
+    saleDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    quantitySold: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+    price_at_sale: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
   },
-  vehicleId: {
-    type: DataTypes.STRING(255),
-    field: 'vehicle_id',  // maps JS "vehicleId" -> DB "vehicle_id"
-    allowNull: false
-  },
-  saleDate: {
-    type: DataTypes.DATE,
-    field: 'sale_date',   // maps JS "saleDate" -> DB "sale_date"
-    allowNull: false
-  },
-  quantitySold: {
-    type: DataTypes.INTEGER,
-    field: 'quantity_sold', // maps JS "quantitySold" -> DB "quantity_sold"
-    allowNull: false,
-    defaultValue: 1
-  },
-  priceAtSale: {
-    type: DataTypes.DECIMAL(10, 2),
-    field: 'price_at_sale', // maps JS "priceAtSale" -> DB "price_at_sale"
-    allowNull: false
+  {
+    tableName: "items_sold",
+    timestamps: false,
   }
-}, {
-  tableName: 'items_sold', 
-  timestamps: false
-});
+);
 
 module.exports = ItemSold;
