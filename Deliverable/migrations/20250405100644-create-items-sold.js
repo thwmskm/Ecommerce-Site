e0eece79b5d06b61cc218636d9035a94d226ee7f
Thomas Kim <thomas.san03@gmail.com>
2025-09-15 14:42:30 -0400

    final commit

--- Deliverable/migrations/20250405100644-create-items-sold.js
+++ Deliverable/migrations/20250405100644-create-items-sold.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("ItemSold", {
+    await queryInterface.createTable("items_sold", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -47,6 +47,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("ItemSold");
+    await queryInterface.dropTable("items_sold");
   },
 };
