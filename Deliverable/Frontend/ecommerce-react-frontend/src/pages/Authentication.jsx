8363311ec8785e4835d35fd67b11912e7dd13ba8
Thomas Kim <thomas.san03@gmail.com>
2025-11-13 23:12:44 -0500

    minor bugs pt2

--- Deliverable/Frontend/ecommerce-react-frontend/src/pages/Authentication.jsx
+++ Deliverable/Frontend/ecommerce-react-frontend/src/pages/Authentication.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const Authentication = () => {
   const [loginData, setLoginData] = useState({ email: "", password: "" });
